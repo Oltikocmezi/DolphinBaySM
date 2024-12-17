c8683fb7ed51e17f533bf23a858dbe99a4e036cc
ShendRudari <shendrudari@gmail.com>
2024-12-17 15:35:21 +0100

    Additional changes
    Instalimi i paketave

--- api/app.js
+++ api/app.js
@@ -3,6 +3,10 @@ import express from "express";
 
 const app = express();
 
+app.use("/api/test", (req,res) => {
+    res.send("It works")
+})
+
 app.listen(8800, () => {
     console.log("server is running");
     
