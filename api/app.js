cc8265a0625607415572c37a5b1f917cdc54188e
Oltikocmezi <147403060+Oltikocmezi@users.noreply.github.com>
2024-12-17 16:04:46 +0100

    Database Connection
    prisma library env file setup

--- api/app.js
+++ api/app.js
@@ -1,14 +1,11 @@
 import express from "express";
 
-
 const app = express();
 
-app.use("/api/test", (req,res) => {
-    res.send("It works")
-})
+app.use("/api/test", (req, res) => {
+  res.send("It works");
+});
 
 app.listen(8800, () => {
-    console.log("server is running");
-    
-
-})
+  console.log("server is running");
+});
