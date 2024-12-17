3c981c47f61427377004c90585165da5f63118dd
jozefkomani <jk64784@ubt-uni.net>
2024-12-17 16:10:21 +0100

    controllers , auth
    logic added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,3 +2,14 @@ export const register = (req, res) => {
     //db operations
     console.log("register controller");
   };
+
+  export const login = (req, res) => {
+    //db operations
+    console.log("login controller");
+  };
+  
+  export const logout = (req, res) => {
+    //db operations
+    console.log("logout controller");
+  };
+  
