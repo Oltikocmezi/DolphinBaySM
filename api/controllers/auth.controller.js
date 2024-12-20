407635808247b5b854f3eea3bfc3a9bf3b4c2980
Oltikocmezi <147403060+Oltikocmezi@users.noreply.github.com>
2024-12-20 15:16:34 +0100

    Database Connection
    Database connected, files structure edited

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,30 @@
-export const register = (req, res) => {
-    //db operations
-    console.log("register controller");
-  };
+import prisma from "../lib/prisma.js";
 
-  export const login = (req, res) => {
-    //db operations
-    console.log("login controller");
-  };
-  
-  export const logout = (req, res) => {
-    //db operations
-    console.log("logout controller");
-  };
-  
+// async function testConnection() {
+//   try {
+//     await prisma.$connect();
+//     console.log("Connected to the database");
+//   } catch (error) {
+//     console.error("Error connecting to the database:", error);
+//   } finally {
+//     await prisma.$disconnect();
+//     console.log("Disconnected from the database");
+//   }
+// }
+
+// testConnection();
+
+export const register = (req, res) => {
+  //db operations
+  console.log("register controller");
+};
+
+export const login = (req, res) => {
+  //db operations
+  console.log("login controller");
+};
+
+export const logout = (req, res) => {
+  //db operations
+  console.log("logout controller");
+};
