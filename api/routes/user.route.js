993677bdac12a6a6ccc2260c8ad8c8e1c5494999
Oltikocmezi <147403060+Oltikocmezi@users.noreply.github.com>
2024-12-24 01:50:27 +0100

    Aditional changes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,14 +1,12 @@
 import express from "express";
 import { getUsers, getUser } from "../controllers/user.controller.js";
 
-import { verifyToken } from "../middleware/verifyToken.js";
+//import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
-router.get("/search/:id", verifyToken, getUser);
+router.get("/search/:id", getUser);
 //router.put("/:id", verifyToken, updateUser);
 
 export default router;
-
-
